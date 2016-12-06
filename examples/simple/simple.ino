f1e5ed14ae0088522d67111480eb1cb7ad9c5501
jadr2ddude <jadr2ddude@gmail.com>
2016-12-06 15:20:37 -0500

    Simplify example

---
+++ examples/simple/simple.ino
@@ -0,0 +1,30 @@
+// Example sketch for interfacing with the DS1302 timekeeping chip.
+//
+// Copyright (c) 2009, Matt Sparks
+// All rights reserved.
+//
+// http://quadpoint.org/projects/arduino-ds1302
+#include <DS1302.h>
+
+void setup() {
+  Serial.begin(9600);
+
+  // Initialize a new chip by turning off write protection and clearing the
+  // clock halt flag. These methods needn't always be called. See the DS1302
+  // datasheet for details.
+  rtc.writeProtect(false);
+  rtc.halt(false);
+
+  // Make a new time object to set the date and time.
+  // Sunday, September 22, 2013 at 01:38:50.
+  Time t(2013, 9, 22, 1, 38, 50, Time::kSunday);
+
+  // Set the time and date on the chip.
+  rtc.time(t);
+}
+
+// Loop and print the time every second.
+void loop() {
+  Serial.println(rtc.time().formatDateTime());  //Print out the time
+  delay(1000);
+}
